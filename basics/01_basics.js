c32aa150de88288f77afd1dcacc1366e77ae55f5
Krupa Isaac <krupaisaac2003@gmail.com>
2026-05-08 09:44:32 +0000

    commiting map.js

---
+++ basics/01_basics.js
@@ -0,0 +1,19 @@
+// const myObject = {
+//     js: 'javascript',
+//     rb: 'ruby',
+//     cpp: 'c++',
+//     ts: 'typescript'
+// }
+
+// for (const sub in myObject) {
+//     console.log(sub,':-', myObject[sub])    
+// }
+
+
+// const coding = ['js', 'python', 'c++']
+
+// coding.forEach(value => {
+//     console.log(value.length);
+// });
+
+
